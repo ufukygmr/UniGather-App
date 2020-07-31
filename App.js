b765f8067e128208569d7547aac1d15a12945811
VengerA <38430063+VengerA@users.noreply.github.com>
2020-07-31 11:24:50 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -13,7 +13,6 @@ import Login from './src/components/login';
 import SignUp from './src/components/signUp';
 
 import SporPage from './src/components/sporPage';
-import YanaciPage from './src/components/YabanciPage';
 import Oneri from './src/components/oneri';
 import MainPage from './src/components/mainPage';
 import NewEvent from './src/components/newEvent';
@@ -21,8 +20,11 @@ import Events from './src/components/events';
 
 import Messeages from './src/components/messeages';
 import YabanciPage from './src/components/YabanciPage';
+import Message from './src/components/messeage';
 
 import MainStore from './src/components/store';
+
+
 console.disableYellowBox = true; 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -41,11 +43,12 @@ function AnaStack() {
         }}
         
       />
-      <Stack.Screen name="Yabancı Dil" component={YabanciPage} options = {{headerStyle: {backgroundColor: '#cae7fd'}, headerBackTitle : " "}} />
-      <Stack.Screen name="Öneri" component={Oneri} />
-      <Stack.Screen name="Spor" component={SporPage} options = {{headerStyle: {backgroundColor: '#fff9cd', shadowColor: 'transparent'}, headerBackTitle : " "}}/>
-      <Stack.Screen name="Events" component={Events} />
-      <Stack.Screen name="NewEvent" component={NewEvent} />
+      <Stack.Screen name="Yabancı Dil" component={YabanciPage} options = {{headerStyle: {backgroundColor: '#cae7fd'},headerTitleStyle: {fontFamily: 'Quicksand'}, headerBackTitle : " "}} />
+      <Stack.Screen name="Öneri" component={Oneri} options = {{headerTitleStyle: {fontFamily: 'Quicksand'},}}/>
+      <Stack.Screen name="Spor" component={SporPage} options = {{headerStyle: {backgroundColor: '#fff9cd', shadowColor: 'transparent'},headerTitleStyle: {fontFamily: 'Quicksand'}, headerBackTitle : " "}}/>
+      <Stack.Screen name="Events" component={Events} options = {{headerTitleStyle: {fontFamily: 'Quicksand'},}} />
+      <Stack.Screen name="NewEvent" component={NewEvent} options = {{headerTitleStyle: {fontFamily: 'Quicksand'},}} />
+      <Stack.Screen name = "Mesaj" component = {Message} options = {{headerStyle: {backgroundColor: '#fff9cd' ,shadowColor: 'transparent'},headerTitleStyle: {fontFamily: 'Quicksand'},title : MainStore.sender , tabBarVisible: 'false'}}/>
     </Stack.Navigator>
   );  
 }
@@ -53,10 +56,24 @@ function AnaStack() {
 function MesajStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name = "Mesajlar" component = {Messeages}/>
+      <Stack.Screen name = "Mesajlar" component = {Messeages} />
+      <Stack.Screen name = "Mesaj" component = {Message} options = {{headerStyle: {fontFamily: 'Quicksand',backgroundColor: '#FF9357' ,shadowColor: 'transparent'},title : MainStore.sender,headerTitleStyle: {fontFamily: 'Quicksand'}, tabBarVisible: 'false'}}/>
     </Stack.Navigator>
   )
 }
+
+MesajStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible = true;
+  for (let i = 0; i < navigation.state.routes.length; i++) {
+    if (navigation.state.routes[i].routeName == "Comments") {
+      tabBarVisible = false;
+    }
+  }
+
+  return {
+    tabBarVisible
+  };
+};
 
 function TabBar() {
   return (
@@ -91,9 +108,9 @@ export default function App() {
     <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Openning" component={Openning} options = {{headerShown: false}} />
-          <Stack.Screen name="Giris" component={Login} options = {{ headerTitle: "", headerBackTitle: " "}} />
-          <Stack.Screen name="Kayit" component={SignUp} options = {{ headerTitle: "", headerBackTitle: " "}} />
-          <Stack.Screen name="App" component= {TabBar} options = {{headerShown : false ,}} />
+          <Stack.Screen name="Giris" component={Login} options = {{ headerTitle: "", headerBackTitle: " ", headerTitleStyle: {fontFamily: 'Quicksand'},}} />
+          <Stack.Screen name="Kayit" component={SignUp} options = {{ headerTitle: "", headerBackTitle: " " ,headerTitleStyle: {fontFamily: 'Quicksand'},}} />
+          <Stack.Screen name="App" component= {TabBar} options = {{headerShown : false ,headerTitleStyle: {fontFamily: 'Quicksand'},}} />
         </Stack.Navigator>
     </NavigationContainer>
   );
